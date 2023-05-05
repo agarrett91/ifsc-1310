fdeda0f9b902bd0acda131a3e2bcd087867fc2f1
agarrett91 <agarrett1@ualr.edu>
2023-05-05 14:52:14 +0000

    Gallery Add, fully working

---
+++ dragon-v2/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      speed: 1500,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      asNavFor: '.slider-nav'
+    });
+    
+    $('.slider-nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: true,
+      centerMode: true,
+      focusOnSelect: true
+    });
+  });
